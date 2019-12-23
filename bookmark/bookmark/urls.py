0ba70824f8e7c4d906b6ddbb804a2cb4a573d05c
djangojeng-e <headfat1218@gmail.com>
2019-12-23 21:39:43 +0900

    Registered bookmark url and finished setting on it.

---
+++ bookmark/bookmark/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import BookmarkListView
+
+urlpatterns = [
+    path('', BookmarkListView.as_view(), name='list')
+]
+
+# bookmark/ 에 접속했을때, BookmarkListView.as_view() 라는 뷰가 호출됨
+## 클래스 베이스는 as_view() 라고 꼭 붙여줘야 정상적으로 동작함.
+# 마지막 인자인 name='list' 는 설정한 이름을 가지고 해당 URL 패턴을 찾을수 있도록 하는 역할
--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Bookmark
+from django.views.generic.list import ListView
 
 # Create your views here.
+
+# def BookmarkListView(request):
+#     pass
+
+# 이번 앱은 모든 뷰를 클래스 형 뷰로 만들것입니다.
+# 뷰에는 함수형 뷰와 클래스형 뷰가 잇다는것을 기억해야합니다.
+
+
+class BookmarkListView(ListView):
+    model = Bookmark
+
+
+# ListView를 상속해서 사용
+# model을 import 해서 사용
