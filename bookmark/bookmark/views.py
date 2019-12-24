3dee04c53ffa742c1c8ee73ce0ba40e45d0f98a9
djangojeng-e <headfat1218@gmail.com>
2019-12-24 21:07:36 +0900

    Added delete function to the bookmark.

--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView
 from .models import Bookmark
 from django.views.generic.list import ListView
 
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -40,3 +40,7 @@ class BookmarkUpdateView(UpdateView):
     fields = ['site_name', 'url']
     template_name_suffix = '_update'
 
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('list')
