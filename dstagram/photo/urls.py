39888497de7742798d69f14a37fe2f45a6d24b4e
djangojeng-e <headfat1218@gmail.com>
2019-12-25 21:04:13 +0900

    Registered base.html ready to be extended.

--- dstagram/photo/urls.py
+++ dstagram/photo/urls.py
@@ -20,13 +20,14 @@ from django.views.generic import DetailView
 from .views import photo_list, CreateView, DeleteView, PhotoDeleteView, PhotoUploadView, PhotoUpdateView
 from .models import Photo
 
-app_name = ['photo']
+
 
 # app_name 은 namespace로 사용되는 값. 템플릿에서 url 템플릿 태그를 사용할때 app_name 값이 설정 되어 있다면
 # [app_name:URL패턴이름] 형태로 사용
 
 # 제너릭 뷰인 DetailView 는 urls.py에서 인라인 코드로 작성 가능. path 함수에 인수로 전달할 때는 as_view안에 클래스 변수들을 설정해 사용
 
+app_name = 'photo'
 
 urlpatterns = [
     path('', photo_list, name='photo_list'),
