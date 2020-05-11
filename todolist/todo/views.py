3d8a69d1f3ffc10942f5f5d58f933fb9079b4dd2
djangojeng-e <headfat1218@gmail.com>
2020-05-12 01:00:42 +0900

    TodoList - List page completed using classbased view.

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import TodoList, TodoList_files, TodoList_images
+from django.views import generic
 
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    context_object_name = 'to_do_list'
+    template_name = 'todo/todo-lists.html' 
+
+    def get_queryset(self):
+        return TodoList.objects.all()
