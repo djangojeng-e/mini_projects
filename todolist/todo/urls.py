f8f317a7e383240a0eeab0ffe87e956504ff0305
djangojeng-e <headfat1218@gmail.com>
2020-05-12 18:21:41 +0900

    TodoList - detail page has been constructed using  DetailView

--- todolist/todo/urls.py
+++ todolist/todo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, DetailView
 
 app_name = 'todo'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='todolist'),
+    path('detail/<int:pk>/', DetailView.as_view(), name="todolist_detail"),
 ]
--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -11,3 +11,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return TodoList.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = TodoList
+    context_object_name = 'todolist'
+    
+    def get_queryset(self):
+        return TodoList.objects.all()
