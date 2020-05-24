e843aa5a7d193b4daf337d5911177fb4379bc8f3
djangojeng-e <headfat1218@gmail.com>
2020-05-24 22:17:24 +0900

    TodoList - deploy-docker shell script has been set

--- todolist/config/settings.py
+++ todolist/config/settings.py
@@ -32,7 +32,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '52.79.175.188',]
+    '52.79.175.188',
+    '*']
 
 
 AUTHENTICATION_BACKENDS = (
--- todolist/deploy-docker.sh
+++ todolist/deploy-docker.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env sh 
 IDENTITY_FILE="$HOME/.ssh/newtodolist.pem"
-HOST="ubuntu@52.79.175.188"
+HOST="ubuntu@3.34.3.36"
 ORIGIN_SOURCE="$HOME/mini_projects/todolist/"
 DEST_SOURCE="/home/ubuntu/todolist/"
 DOCKER_REPO="headfat1218/todolist"
@@ -21,18 +21,34 @@ echo "pip freeze"
 "$HOME"/.pyenv/versions/3.8.0/envs/to-do-list/bin/pip freeze > "$HOME"/mini_projects/todolist/requirements.txt
 
 
-
-
 # docker build 
 echo "Docker build"
-docker build -q -t ${DOCKER_REPO} -f Dockerfile "$HOME"/mini_projects/todolist/
+docker build -t todolist ~/mini_projects/todolist
+
+# docker image tag 
+echo "Docker image tag"
+docker tag todolist:latest headfat1218/todolist:latest
 
 # docker push 
 echo "docker push"
-docker push ${DOCKER_REPO}
+docker push headfat1218/todolist:latest
 
 # docker stop 
 echo "docker stop"
 ${SSH_CMD} -C "sudo docker stop todolist"
 
-# 
+# docker pull 
+echo "docker pull"
+${SSH_CMD} -C "sudo docker pull headfat1218/todolist:latest"
+
+# screen 에서 docker run 
+echo "screen settings"
+${SSH_CMD} -C 'screen -X -S docker quit'
+
+# screen 실행 
+${SSH_CMD} -C 'screen -S docker -d -m'
+
+# 실행중인 세션에 명령어 전달 
+${SSH_CMD} -C "screen -r docker -X stuff 'sudo docker run --rm -it -p 80:8000 --name todolist headfat1218/todolist:latest\n'"
+
+echo "Finish!"
